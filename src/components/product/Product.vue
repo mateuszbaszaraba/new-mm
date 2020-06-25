2ba553f1e37c3312a66065c6c3c576794f2b955f
Matt <mateuszbaszaraba@outlook.com>
2020-06-25 13:07:40 +0200

    upgraded rendering

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
-        <p>{{ product.name }}</p>
+    <div class="prod-cont">
+        <router-link :to="'/nasze-produkty' + product.id"><img :src="getImgUrl(product.id)" /></router-link>
+        <div class="prod-cont-txt">
+            <p>{{ product.name }}</p>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-      props: ['product']  
+      props: ['product'],
+      methods: {
+          getImgUrl(pic) {
+              return require('../../assets/furn/prod' + pic + '.png')
+          }
+      }
     }
 </script>
+
+<style lang="scss">
+
+    .prod-cont {
+        width: 25vw;
+        height: 40vh;
+        background-color: red;
+    }
+    .prod-cont img {
+        width: 20vw;
+    }
+
+</style>
--- src/data/furnitures.js
+++ src/data/furnitures.js
@@ -1,4 +1,5 @@
 export default [
-    { id: 1, name: 'Kanapa Sydney', dim1: '2,25m', dim2: '1,45m x 1,95m', photo: './assets/furn/kanapa1.png'},
+    { id: 1, name: 'Kanapa Sydney', dim1: '2,25m', dim2: '1,45m x 1,95m'},
+    { id: 2, name: 'Kanapaaa Sysdadsdney', dim1: '2,25m', dim2: '1,45m x 1,95m'},
     
 ]
