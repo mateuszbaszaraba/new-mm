891268fee541be77eb61d1caf95f957c48cc71d7
Matt <mateuszbaszaraba@outlook.com>
2020-06-29 10:21:26 +0200

    done styling details

--- src/components/product/ProductData.vue
+++ src/components/product/ProductData.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        {{loadedProd.name}}
-        {{loadedProd.dim1}}
-        {{loadedProd.dim2}}
+    <div class="prod-data">
+        <img :src="getImgUrl(loadedProd.id)" class="imgPD" />
+        <div class="prod-data-txt">
+            <p class="dp1">{{ loadedProd.name }}</p>
+            <p class="dp2">Długość: {{ loadedProd.dim1 }}</p>
+            <p class="dp2">Powierzchnia Spania: {{ loadedProd.dim2 }}</p>
+        </div>
     </div>
 </template>
 
@@ -11,5 +14,52 @@ export default {
     props: {
         loadedProd: Object
     },
+    methods: {
+        getImgUrl(pic) {
+              return require('../../assets/furn/prod' + pic + '.png')
+        }
+    }
 }
 </script>
+
+<style lang="scss">
+
+    .prod-data {
+        width: 90vw;
+        height: 40vh;
+        margin: 2vh 5vw 0 5vw;
+        background-color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .prod-data-txt {
+        width: 40vw;
+        height: 30vh;
+        height: auto;
+        float: left;
+        border-left: 1px solid #5E5E5E;
+        margin: 0 0 0 1vw;
+        padding-left: 5vw;
+    }
+
+    .imgPD {
+        width:30vw;
+        float: left;
+        padding: 2vh 3vw 0 10vw;
+    }
+
+    .dp2 {
+        font-weight: 300; 
+        color: #5E5E5E; 
+        font-size: 1.5vw
+    }
+
+    .dp1 {
+        font-weight: 500; 
+        color: #5E5E5E; 
+        font-size: 1.9vw
+    }
+
+</style>
--- src/components/product/ProductDetail.vue
+++ src/components/product/ProductDetail.vue
@@ -2,13 +2,20 @@
     <div class="prod-det">
         <app-header style="background-color: black"></app-header>
 
-        <div class="txt">
-            
-        </div>
-
         <app-productdata 
-            style="margin-top: 20vh; position: absolute"
+            style="margin-top: 30vh; position: absolute"
             :loadedProd="loadedProd"></app-productdata>
+
+        <router-link class="prod-data-contact" tag="div" to="/kontakt">
+            <p>MASZ PYTANIA? ZADZWOŃ</p>
+        </router-link>
+
+        <div class="footer4">
+            <router-link to="/"><img src="../../assets/logo4.png" /></router-link>
+            <div class="footer-txt4">
+                MatMeble© Wszelkie prawa zastrzeżone
+            </div>
+        </div>
     </div>
 </template>
 
@@ -30,3 +37,43 @@
         }
     }
 </script>
+
+<style lang="scss">
+
+    .prod-det {
+        width: 100vw;
+        height: 100vh;
+        background-color: #E8E8E8;
+    }
+
+    .prod-data-contact {
+        width: 100vw;
+        height: 5vh;
+        margin-top: 84vh;
+        position: absolute;
+        background-color: #E4C68B;
+        color: white;
+        font-size: 1vw;
+        text-align: center;
+        padding: 0.1vh 0 1vh 0;
+        cursor: pointer;
+    }
+
+    .footer4 {
+        background-color: #000000;
+        margin-top: 90vh;
+        text-align: center;
+        height: 35vh;
+        color: white;
+        padding-top: 2vh;
+        position: absolute;
+        width: 100vw;
+    }
+
+    .footer4 img {
+        width: 6vw;
+        height: auto;
+        padding: 10vh 0 5vh 0;
+    }
+
+</style>
